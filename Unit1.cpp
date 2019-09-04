a62305a196147f303517f353bc8ce872054bddf9
Alexell <alexell@alexell.ru>
2019-09-04 21:06:53 +0300

    Загрузка проекта

---
+++ Unit1.cpp
@@ -0,0 +1,217 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+
+#include "IniFiles.hpp"
+//#include <stdlib.h>
+#include <Tlhelp32.h>
+//#include <fstream.h>
+
+#include "Unit1.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TMainForm *MainForm;
+TMemIniFile* Settings;
+int pid;
+bool started = false;
+//---------------------------------------------------------------------------
+__fastcall TMainForm::TMainForm(TComponent* Owner)
+	: TForm(Owner)
+{
+	MainForm->Caption=Application->Title;
+
+    //Загружаем настройки из INI файла
+	if (FileExists(ExtractFilePath(Application->ExeName)+"settings.ini"))
+	{
+		Settings = new TMemIniFile(ExtractFilePath(Application->ExeName)+"settings.ini",TEncoding::UTF8);
+		IPEdit->Text = Settings->ReadString("Server","IP","");
+		PortEdit->Text = Settings->ReadString("Server","Port","");
+		FileEdit->Text = Settings->ReadString("Server","Exe","");
+		CmdMemo->Text = Settings->ReadString("Server","Cmd","");
+		IntEdit->Text = Settings->ReadString("Options","Interval","");
+		AutostartCheck->State=Settings->ReadBool("Options","Autostart",0);
+		HideCheck->State=Settings->ReadBool("Options","Minimized",0);
+		delete Settings;
+		Timer->Interval=StrToInt(IntEdit->Text)*1000;
+	}
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TMainForm::FormShow(TObject *Sender)
+{
+	AboutLabel->Caption = "Автор: Alexell | Сайт: alexell.ru";
+	if(HideCheck->Checked)
+	{
+		Application->Minimize();
+	}
+	if(AutostartCheck->Checked)
+	{
+		StartButton->Click();
+	}
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TMainForm::FileButtonClick(TObject *Sender)
+{
+	if(OpenDialog->Execute())
+	{
+		if (ExtractFileName(OpenDialog->FileName) != "srcds.exe")
+		{
+			Application->MessageBox(L"Указан неверный файл!", Application->Title.w_str(), MB_OK | MB_ICONERROR);
+			return;
+		}
+		FileEdit->Text = OpenDialog->FileName;
+	}
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TMainForm::StartButtonClick(TObject *Sender)
+{
+	if (started == false)
+	{
+		//Обработка полей
+		if (IntEdit->Text == "" || FileEdit->Text == "" || CmdMemo->Text == "" || CmdMemo->Text.Pos("..."))
+		{
+			Application->MessageBox(L"Не заполнены настройки!", Application->Title.w_str(), MB_OK | MB_ICONERROR);
+			return;
+		}
+		if (StrToInt(IntEdit->Text)<60)
+		{
+			Application->MessageBox(L"Минимальный интервал мониторинга: 60 секунд.", Application->Title.w_str(), MB_OK | MB_ICONERROR);
+			IntEdit->Text = "60";
+			return;
+		}
+		if(CmdMemo->Text.SubString(0,9) != "srcds.exe")
+		{
+			Application->MessageBox(L"Командная строка должна начинаться с \"srcds.exe\"!", Application->Title.w_str(), MB_OK | MB_ICONERROR);
+            CmdMemo->Text = "srcds.exe " + CmdMemo->Text;
+			return;
+		}
+
+		//Старт
+		Settings = new TMemIniFile(ExtractFilePath(Application->ExeName)+"settings.ini",TEncoding::UTF8);
+		Settings->WriteString("Server","IP",IPEdit->Text);
+		Settings->WriteString("Server","Port",PortEdit->Text);
+		Settings->WriteString("Server","Exe",FileEdit->Text);
+		Settings->WriteString("Server","Cmd",CmdMemo->Text);
+		Settings->WriteString("Options","Interval",IntEdit->Text);
+		Settings->WriteBool("Options","Autostart",AutostartCheck->State);
+		Settings->WriteBool("Options","Minimized",HideCheck->State);
+		Settings->UpdateFile();
+		delete Settings;
+
+		IPEdit->Enabled = false;
+		PortEdit->Enabled = false;
+		FileEdit->Enabled = false;
+		FileButton->Enabled = false;
+		CmdMemo->Enabled = false;
+		IntEdit->Enabled = false;
+		AutostartCheck->Enabled = false;
+		HideCheck->Enabled = false;
+		Timer->Interval=StrToInt(IntEdit->Text)*1000;
+		IdTCPClient->Host=IPEdit->Text;
+		IdTCPClient->Port=StrToInt(PortEdit->Text);
+		Timer->Enabled = true;
+		started = true;
+		StartButton->Caption = "Остановить мониторинг";
+	}
+	else
+	{
+		//Стоп
+		IPEdit->Enabled = true;
+		PortEdit->Enabled = true;
+		FileEdit->Enabled = true;
+		FileButton->Enabled = true;
+		CmdMemo->Enabled = true;
+		IntEdit->Enabled = true;
+		AutostartCheck->Enabled = true;
+		HideCheck->Enabled = true;
+		StartButton->Enabled = true;
+		Timer->Enabled = false;
+		started = false;
+		StartButton->Caption = "Начать мониторинг";
+	}
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TMainForm::TimerTimer(TObject *Sender)
+{
+	//Мониторинг доступности IP:Port
+	char *cmd;
+	Timer->Enabled = false;
+	try
+	{
+		IdTCPClient->Connect();
+	}
+	catch(...) //Порт недоступен
+	{
+		//Проверяем наличие процесса и убиваем его
+        pid = 0;
+		cmd = AnsiString(ExtractFileName(FileEdit->Text)).c_str();
+		pid = IsProcessRunning(cmd);
+		if (pid != 0)
+		{
+			KillProcess(pid);
+		}
+
+		//Запускаем сервер
+		SetCurrentDir(ExtractFileDir(FileEdit->Text));
+		cmd = AnsiString(ExtractFileName(FileEdit->Text)+" "+CmdMemo->Text).c_str();
+		WinExec(cmd,SW_SHOW);
+	}
+	if(IdTCPClient->Connected()) //Порт доступен
+	{
+		IdTCPClient->Disconnect();
+	}
+	Timer->Enabled = true;
+}
+//---------------------------------------------------------------------------
+
+int TMainForm::IsProcessRunning( char * pName )
+{
+	int id_p;
+	strlwr( pName );
+	HANDLE snapshot = CreateToolhelp32Snapshot( TH32CS_SNAPPROCESS, 0 );
+	PROCESSENTRY32 pe;
+	char pExeName[256];
+	for( BOOL r=Process32First(snapshot, &pe); r; r=Process32Next(snapshot, &pe) )
+	{
+		if( strcmp( pe.szExeFile, pName ) == 0 )
+		{
+			id_p=pe.th32ProcessID;
+			CloseHandle(snapshot);
+			return id_p;
+		}
+	}
+	CloseHandle(snapshot);
+	return 0;
+}
+//---------------------------------------------------------------------------
+
+void TMainForm::KillProcess(int id)
+{
+   HANDLE ps = OpenProcess(1, false, id);
+   if (ps) TerminateProcess(ps, -9);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TMainForm::CmdMemoClick(TObject *Sender)
+{
+    //Очистка поля командной строки при первой настройке
+	if (CmdMemo->Text.Pos("..."))
+	{
+        CmdMemo->Text = "";
+	}
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TMainForm::IPEditKeyPress(TObject *Sender, System::WideChar &Key)
+{
+    //Фильтр для IP адреса
+	if((Key >= '0') && (Key <= '9') || Key == '.' || Key == VK_BACK){}
+	else Key = 0;
+}
+//---------------------------------------------------------------------------
+
---
+++ Unit1.h
@@ -0,0 +1,58 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+#include <Vcl.ExtCtrls.hpp>
+#include <Vcl.Dialogs.hpp>
+#include <IdBaseComponent.hpp>
+#include <IdComponent.hpp>
+#include <IdTCPClient.hpp>
+#include <IdTCPConnection.hpp>
+#include <IdUDPBase.hpp>
+#include <IdUDPClient.hpp>
+#include <IdAntiFreeze.hpp>
+#include <IdAntiFreezeBase.hpp>
+#include <Vcl.AppEvnts.hpp>
+//---------------------------------------------------------------------------
+class TMainForm : public TForm
+{
+__published:	// IDE-managed Components
+	TLabeledEdit *IPEdit;
+	TLabeledEdit *PortEdit;
+	TLabeledEdit *IntEdit;
+	TCheckBox *HideCheck;
+	TLabeledEdit *FileEdit;
+	TOpenDialog *OpenDialog;
+	TButton *FileButton;
+	TTimer *Timer;
+	TButton *StartButton;
+	TIdTCPClient *IdTCPClient;
+	TIdAntiFreeze *IdAntiFreeze;
+	TMemo *CmdMemo;
+	TLabel *CmdLabel;
+	TCheckBox *AutostartCheck;
+	TTrayIcon *Tray;
+	TLabel *AboutLabel;
+	TLabel *ActionLabel;
+	void __fastcall FileButtonClick(TObject *Sender);
+	void __fastcall StartButtonClick(TObject *Sender);
+	void __fastcall TimerTimer(TObject *Sender);
+	void __fastcall FormShow(TObject *Sender);
+	void __fastcall CmdMemoClick(TObject *Sender);
+	void __fastcall IPEditKeyPress(TObject *Sender, System::WideChar &Key);
+
+private:	// User declarations
+public:		// User declarations
+	int IsProcessRunning(char *pName);
+	void KillProcess(int id);
+	__fastcall TMainForm(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TMainForm *MainForm;
+//---------------------------------------------------------------------------
+#endif
