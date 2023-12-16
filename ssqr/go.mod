b3fcd84344ba1f22e14a54eec6f5ed184227213d
Alexell <alexell@alexell.ru>
2023-12-16 11:19:51 +0600

    Доработка SSQR
    - в rconGo нет таймаута для query запросов, поэтому для таких запросов используем библиотеку go-a2s
    - другие мелкие правки

--- ssqr/go.mod
+++ ssqr/go.mod
@@ -2,4 +2,7 @@ module ssqr
 
 go 1.20
 
-require github.com/xv-chang/rconGo v0.0.0-20210706051530-221338f352d6 // indirect
+require (
+	github.com/rumblefrog/go-a2s v1.0.2
+	github.com/xv-chang/rconGo v0.0.0-20210706051530-221338f352d6
+)
--- ssqr/ssqr.go
+++ ssqr/ssqr.go
@@ -4,62 +4,55 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"time"
 	"os"
+	"github.com/rumblefrog/go-a2s"
 	"github.com/xv-chang/rconGo/core"
 )
 
 type Action string
 
 const (
-	Rcon  Action = "rcon"
-	Query Action = "query"
+	Rcon	Action = "rcon"
+	Query	Action = "query"
 )
 
 var (
+	ErrGlobal = errors.New("error")
 	ErrNotAuth = errors.New("error_password")
 )
 
 type InformationServer struct {
-	Map        string `json:"map"`
-	Players    int    `json:"players"`
-	MaxPlayers int    `json:"max_players"`
-	Password   bool   `json:"password"`
+	Map			string `json:"map"`
+	Players		int	`json:"players"`
+	MaxPlayers	int	`json:"max_players"`
+	Password	bool   `json:"password"`
 }
 
 func main() {
 	args := os.Args
 
 	if len(args) < 3 {
-		fmt.Println("error")
+		fmt.Println(ErrGlobal)
 		return
 	}
 
 	action := Action(args[1])
 	if action == Rcon && len(args) < 5 {
-		fmt.Println("error")
+		fmt.Println(ErrGlobal)
 		return
 	}
 
 	ip := args[2]
 	port := args[3]
 
-	command := ""
-	password := ""
 	if action == Rcon {
-		command = args[4]
-		password = args[5]
-	}
-
-	if action == Rcon {
+		command := args[4]
+		password := args[5]
 		result, errResult := handleRcon(ip, port, command, password)
 
 		if errResult != nil {
-			if errors.As(errResult, &ErrNotAuth) {
-				fmt.Println(errResult.Error())
-				return
-			}
-
-			fmt.Println("error")
+			fmt.Println(errResult)
 			return
 		}
 
@@ -70,18 +63,18 @@ func main() {
 		result, errResult := handleQuery(ip, port)
 
 		if result == nil {
-			fmt.Println("error")
+			fmt.Println(ErrGlobal)
 			return
 		}
 
 		if errResult != nil {
-			fmt.Println("error")
+			fmt.Println(ErrGlobal)
 			return
 		}
 
 		body, err := json.Marshal(result)
 		if err != nil {
-			fmt.Println("error")
+			fmt.Println(ErrGlobal)
 			return
 		}
 
@@ -92,7 +85,8 @@ func main() {
 func handleRcon(ip, port, command, password string) (string, error) {
 	defer func() {
 		if err := recover(); err != nil {
-			fmt.Println("error")
+			fmt.Println(ErrGlobal)
+			os.Exit(1) // костыль
 		}
 	}()
 
@@ -107,7 +101,7 @@ func handleRcon(ip, port, command, password string) (string, error) {
 
 	result, errQuery := client.ExecCommand(command)
 	if errQuery != nil {
-		return "", errQuery
+		return "", ErrGlobal
 	}
 
 	return result, nil
@@ -115,14 +109,24 @@ func handleRcon(ip, port, command, password string) (string, error) {
 
 func handleQuery(ip, port string) (*InformationServer, error) {
 	host := fmt.Sprintf("%s:%s", ip, port)
-	client := core.NewServerQuery(host)
+	client, err := a2s.NewClient(
+		host,
+		a2s.TimeoutOption(time.Second * 2),
+	)
+	if err != nil {
+		return nil, err
+	}
 	defer client.Close()
-	result := client.GetInfo()
+	
+	result, err := client.QueryInfo()
+	if err != nil {
+		return nil, err
+	}
 
 	return &InformationServer{
-		Map:        result.Map,
-		Players:    int(result.Players),
-		MaxPlayers: int(result.MaxPlayers),
-		Password:   result.Visibility != 0,
+		Map:		result.Map,
+		Players:	int(result.Players),
+		MaxPlayers:	int(result.MaxPlayers),
+		Password:	result.Visibility,
 	}, nil
 }
